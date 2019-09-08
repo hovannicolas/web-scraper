15f40d8e2af146f1a3d2026689b09cdbae393043
Nicolas Hovan <hovan.nicolas@gmail.com>
2019-09-08 09:14:43 -0700

    Add web-crawling functionality

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 from csv import writer
 
-response = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(response.text, "html.parser")
-quotes = (soup.find_all(class_="quote"))
 all_quotes =[]
+base_url = "http://quotes.toscrape.com"
+url = "/page/1"
 
-for quote in quotes:
-    all_quotes.append({
+while url:
+    response = requests.get(f"{base_url}{url}")
+    print(f"Now scraping {base_url}{url}...")
+    soup = BeautifulSoup(response.text, "html.parser")
+    quotes = (soup.find_all(class_="quote"))
+
+    for quote in quotes:
+        all_quotes.append({
         "text": quote.find(class_="text").get_text(),
         "author": quote.find(class_="author").get_text(),
         "bio-href": quote.find(class_="a")
-    })
+        })
+    next_btn = soup.find(class_="next")
+    url = next_btn.find("a")["href"] if next_btn else None
 print(all_quotes)
+
+
+
 
 
 
